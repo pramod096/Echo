0a50c98fe4abc1c746ad7f3568b68e47f8e3b8d8
Pramod <pramodreddy096@gmail.com>
2020-04-17 11:06:35 +0530

    Added plain screen adapter.

--- app/src/main/java/com/example/echo/adapters/MainScreenAdapter.kt
+++ app/src/main/java/com/example/echo/adapters/MainScreenAdapter.kt
@@ -25,33 +25,43 @@ class MainScreenAdapter(_songDetails: ArrayList<Songs>, _context: Context) : Rec
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        val songObject = songDetails?.get(position)
-        holder.trackTitle?.text = songObject?.songTitle
-        holder.trackArtist?.text = songObject?.artist
-        holder.contentHolder?.setOnClickListener {
-            val songPlayingFragment = SongPlayingFragment()
-            val args = Bundle()
-            args.putString("songArtist", songObject?.artist)
-            args.putString("songTitle", songObject?.songTitle)
-            args.putString("path", songObject?.songData)
-            args.putInt("songId", songObject?.songID?.toInt() as Int)
-            args.putInt("songPosition", position)
-            args.putParcelableArrayList("songData", songDetails)
-            songPlayingFragment.arguments = args
-            (mContext as FragmentActivity).supportFragmentManager
-                .beginTransaction()
-                .replace(R.id.details_fragment, songPlayingFragment)
-                .addToBackStack("SongPlayingFragment")
-                .commit()
-            if( SongPlayingFragment.Statified.currentSongHelper?.isPlaying == true) {
-                SongPlayingFragment.Statified.mediaplayer?.reset()
+        if(position == (songDetails as ArrayList<Songs>).size){
+            
+        }
+            else {
+            val songObject = songDetails?.get(position)
+            holder.trackTitle?.text = songObject?.songTitle
+            holder.trackArtist?.text = songObject?.artist
+            holder.contentHolder?.setOnClickListener {
+                val songPlayingFragment = SongPlayingFragment()
+                val args = Bundle()
+                args.putString("songArtist", songObject?.artist)
+                args.putString("songTitle", songObject?.songTitle)
+                args.putString("path", songObject?.songData)
+                args.putInt("songId", songObject?.songID?.toInt() as Int)
+                args.putInt("songPosition", position)
+                args.putParcelableArrayList("songData", songDetails)
+                songPlayingFragment.arguments = args
+                (mContext as FragmentActivity).supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.details_fragment, songPlayingFragment)
+                    .addToBackStack("SongPlayingFragment")
+                    .commit()
+                if (SongPlayingFragment.Statified.currentSongHelper?.isPlaying == true) {
+                    SongPlayingFragment.Statified.mediaplayer?.reset()
+                }
             }
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        val itemView = LayoutInflater.from(parent.context)
-            .inflate(R.layout.row_custom_mainscreen_adapter,parent,false)
+        var itemView : View
+        if(viewType == R.layout.row_custom_mainscreen_adapter)
+            itemView = LayoutInflater.from(parent.context)
+                .inflate(R.layout.row_custom_mainscreen_adapter,parent,false)
+        else
+            itemView = LayoutInflater.from(parent.context)
+                .inflate(R.layout.plain_custom_mainscreen_adapter,parent,false)
 
         return MyViewHolder(itemView)
     }
@@ -60,10 +70,16 @@ class MainScreenAdapter(_songDetails: ArrayList<Songs>, _context: Context) : Rec
         return if(songDetails == null){
             0
         }else{
-            (songDetails as ArrayList<Songs>).size
+            (songDetails as ArrayList<Songs>).size + 1
         }
     }
 
+    override fun getItemViewType(position: Int): Int {
+        return if(position == (songDetails as ArrayList<Songs>).size)
+            R.layout.plain_custom_mainscreen_adapter
+        else
+            R.layout.row_custom_mainscreen_adapter
+    }
     class MyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         var trackTitle: TextView? = null
         var trackArtist: TextView? = null
